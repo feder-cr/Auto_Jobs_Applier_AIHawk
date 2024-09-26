e786bae1dc29b3676db80c384a108fb79374713b
Tilak Patel <ptilak142@gmail.com>
2024-09-25 23:26:00 -0400

    add partial extractor

--- src/extractors/extraction_chains.py
+++ src/extractors/extraction_chains.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from src.job import Job
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 # An interface that defines different extraction strategies for the linkedin jobs page.
@@ -74,4 +76,123 @@ class Extractor1(Extractor):
         return job_title, company, job_location, link, apply_method
 
 
-EXTRACTORS = [Extractor1()]
+class Extractor2(Extractor):
+    def get_job_list(self, driver) -> list[Job]:
+        try:
+            # Wait for the job list container to be present
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "scaffold-layout__list-container")
+                )
+            )
+
+            # Find the job list container
+            job_list_container = driver.find_element(
+                By.CLASS_NAME, "scaffold-layout__list-container"
+            )
+
+            # Find all job items within the container
+            job_list_elements = job_list_container.find_elements(
+                By.CSS_SELECTOR,
+                "li.ember-view.jobs-search-results__list-item.occludable-update.p0.relative.scaffold-layout__list-item",
+            )
+
+            print(f"Number of job elements found: {len(job_list_elements)}")
+
+            if not job_list_elements:
+                raise Exception("No job elements found on page")
+
+            job_list = [
+                Job(*self.extract_job_information_from_tile(job_element))
+                for job_element in job_list_elements
+            ]
+            return job_list
+        except Exception as e:
+            return []
+
+    def extract_job_information_from_tile(self, job_tile):
+        job_title, company, job_location, apply_method, link = "", "", "", "", ""
+        try:
+            job_title = job_tile.find_element(
+                By.CSS_SELECTOR, "a.job-card-list__title"
+            ).text
+            link = (
+                job_tile.find_element(By.CSS_SELECTOR, "a.job-card-list__title")
+                .get_attribute("href")
+                .split("?")[0]
+            )
+            company = job_tile.find_element(
+                By.CSS_SELECTOR, ".job-card-container__primary-description"
+            ).text
+            job_location = job_tile.find_element(
+                By.CSS_SELECTOR, ".job-card-container__metadata-item"
+            ).text
+            apply_method = job_tile.find_element(
+                By.CSS_SELECTOR, ".job-card-container__apply-method"
+            ).text
+        except NoSuchElementException as e:
+            print(f"Error extracting job information: {str(e)}")
+
+        return job_title, company, job_location, link, apply_method
+
+# extractor2 isn't really working for me.
+class Extractor3(Extractor):
+    def get_job_list(self, driver) -> list[Job]:
+        try:
+            # Wait for the job list container to be present
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "scaffold-layout__list-container")
+                )
+            )
+
+            # Find the job list container
+            job_list_container = driver.find_element(
+                By.CLASS_NAME, "scaffold-layout__list-container"
+            )
+
+            # Find all job items within the container
+            job_list_elements = job_list_container.find_elements(
+                By.CSS_SELECTOR,
+                "li.ember-view.jobs-search-results__list-item.occludable-update.p0.relative.scaffold-layout__list-item",
+            )
+
+            print(f"Number of job elements found: {len(job_list_elements)}")
+
+            if not job_list_elements:
+                raise Exception("No job elements found on page")
+
+            job_list = [
+                Job(*self.extract_job_information_from_tile(job_element))
+                for job_element in job_list_elements
+            ]
+            return job_list
+        except Exception as e:
+            return []
+
+    def extract_job_information_from_tile(self, job_tile):
+        job_title, company, job_location, apply_method, link = "", "", "", "", ""
+        try:
+            job_title = job_tile.find_element(
+                By.CSS_SELECTOR, "job-card-job-posting-card-wrapper__title"
+            ).text
+            link = (
+                job_tile.find_element(By.CSS_SELECTOR, "job-card-job-posting-card-wrapper__card-link")
+                .get_attribute("href")
+            )
+            company = job_tile.find_element(
+                By.CSS_SELECTOR, ".job-card-container__primary-description"
+            ).text
+            job_location = job_tile.find_element(
+                By.CSS_SELECTOR, ".job-card-container__metadata-item"
+            ).text
+            apply_method = job_tile.find_element(
+                By.CSS_SELECTOR, ".job-card-container__apply-method"
+            ).text
+        except NoSuchElementException as e:
+            print(f"Error extracting job information: {str(e)}")
+
+        return job_title, company, job_location, link, apply_method
+
+
+EXTRACTORS = [Extractor1(), Extractor2()]
